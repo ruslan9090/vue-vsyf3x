f91d2c86a58532b73440c4d2ed46c9bcdee19c6b
ruslan9090 <ruslanv90@bk.ru>
2022-09-19 15:24:10 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-u4bqfe
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-u4bqfe)
---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="app">
+    <post-form />
+    <post-list />
+  </div>
+</template>
+
+<script>
+import PostForm from './components/PostForm';
+import PostList from './components/PostList';
+
+export default {
+  components: {
+    PostList,
+    PostForm,
+  },
+  data() {
+    return {
+      posts: [
+        { id: 1, title: 'JS', body: 'Описание' },
+        { id: 2, title: 'JS-2', body: 'Описание поста 2' },
+        { id: 3, title: 'JS-3', body: 'Описание поста 3' },
+        { id: 4, title: 'JS-4', body: 'Описание поста 4' },
+      ],
+      title: '',
+      body: '',
+    };
+  },
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      };
+      this.posts.push(newPost);
+      this.title = '';
+      this.body = '';
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.app {
+  padding: 20px;
+}
+</style>
